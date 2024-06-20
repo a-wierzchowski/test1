0f6996679ee70720204326df8668d3b13639ee56
Aleksander Wierzchowski <a.wierzchowski@proton.me>
2024-06-20 10:39:44 +0200

    commit2

--- projetk1.cpp
+++ projetk1.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int silnia (int n){
+	if(n < 1)
+		return 0;
+	int result = 1;
+	while(n != 0){
+		result *= n;
+	       n--;	
+	}
+	return result;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
@@ -12,8 +23,11 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
-        cout << "0. Wyjscie" << endl;
+        cout << "1. Silnia" << endl;
+	cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+	if(wyjscie == 1)
+		cout << silnia(a);
     } while(wyjscie != 0);
     return 0;
 }
