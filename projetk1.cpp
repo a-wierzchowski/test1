192eec5feccf2a1833130399d30844f01381f8c0
Aleksander Wierzchowski <a.wierzchowski@proton.me>
2024-06-21 09:05:55 +0200

    commit4

--- projetk1.cpp
+++ projetk1.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+bool czyPierwsza(int n){
+	if(n < 2)
+		return false;
+	for(int i=2; i*i<=n; i++){
+		if(n%i == 0)
+			return false;
+	}
+	return true;
+}
+
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
